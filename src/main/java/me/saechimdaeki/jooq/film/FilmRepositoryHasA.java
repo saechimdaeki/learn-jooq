64bb555b987e6bf3a7c2fa09624f3aa2b2158452
saechimdaeki/junseong <anima94@kakao.com>
2024-06-21 09:45:04 +0900

    study: generate dao 사용하기

--- src/main/java/me/saechimdaeki/jooq/film/FilmRepositoryHasA.java
+++ src/main/java/me/saechimdaeki/jooq/film/FilmRepositoryHasA.java
@@ -1,9 +1,15 @@
 package me.saechimdaeki.jooq.film;
 
+import java.util.List;
+
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.generated.tables.JActor;
 import org.jooq.generated.tables.JFilm;
+import org.jooq.generated.tables.JFilmActor;
 import org.jooq.generated.tables.daos.FilmDao;
+import org.jooq.generated.tables.pojos.Film;
+import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,5 +21,38 @@ public class FilmRepositoryHasA {
     public FilmRepositoryHasA(Configuration configuration, DSLContext dslContext) {
         this.filmDao = new FilmDao(configuration);
         this.dslContext = dslContext;
+    }
+
+    public Film findById(Long id) {
+        return filmDao.fetchOneByJFilmId(id);
+    }
+
+    public List<Film> fetchRangeOfLength(Integer start, Integer end) {
+        return filmDao.fetchRangeOfJLength(start, end);
+    }
+
+    public SimpleFilmInfo findByIdAsSimpleFilmInfo(Long id) {
+        return dslContext.select(FILM.FILM_ID, FILM.TITLE, FILM.DESCRIPTION)
+                         .from(FILM)
+                         .where(FILM.FILM_ID.eq(id))
+                         .fetchOneInto(SimpleFilmInfo.class);
+    }
+
+    public List<FilmWithActor> findFilmWithActorsList(Long page, Long pageSize) {
+        final JFilmActor FILM_ACTOR = JFilmActor.FILM_ACTOR;
+        final JActor ACTOR = JActor.ACTOR;
+        return dslContext.select(
+                                 DSL.row(FILM.fields()),
+                                 DSL.row(FILM_ACTOR.fields()),
+                                 DSL.row(ACTOR.fields())
+                         )
+                         .from(FILM)
+                         .join(FILM_ACTOR)
+                         .on(FILM.FILM_ID.eq(FILM_ACTOR.FILM_ID))
+                         .join(ACTOR)
+                         .on(FILM_ACTOR.ACTOR_ID.eq(ACTOR.ACTOR_ID))
+                         .limit(pageSize)
+                         .offset((page - 1) * pageSize)
+                         .fetchInto(FilmWithActor.class);
     }
 }
--- src/main/java/me/saechimdaeki/jooq/film/FilmRepositoryIsA.java
+++ src/main/java/me/saechimdaeki/jooq/film/FilmRepositoryIsA.java
@@ -1,11 +1,16 @@
 package me.saechimdaeki.jooq.film;
 
+import java.util.List;
+
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.generated.tables.JActor;
 import org.jooq.generated.tables.JFilm;
+import org.jooq.generated.tables.JFilmActor;
 import org.jooq.generated.tables.daos.FilmDao;
+import org.jooq.generated.tables.pojos.Film;
+import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public class FilmRepositoryIsA extends FilmDao {
@@ -15,5 +20,37 @@ public class FilmRepositoryIsA extends FilmDao {
     public FilmRepositoryIsA(Configuration configuration, DSLContext dslContext) {
         super(configuration);
         this.dslContext = dslContext;
+    }
+
+    public Film findById(Long id) {
+        return dslContext.select(FILM.fields())
+                         .from(FILM)
+                         .where(FILM.FILM_ID.eq(id))
+                         .fetchOneInto(Film.class);
+    }
+
+    public SimpleFilmInfo findByIdAsSimpleFilmInfo(Long id) {
+        return dslContext.select(FILM.FILM_ID, FILM.TITLE, FILM.DESCRIPTION)
+                         .from(FILM)
+                         .where(FILM.FILM_ID.eq(id))
+                         .fetchOneInto(SimpleFilmInfo.class);
+    }
+
+    public List<FilmWithActor> findFilmWithActorsList(Long page, Long pageSize) {
+        final JFilmActor FILM_ACTOR = JFilmActor.FILM_ACTOR;
+        final JActor ACTOR = JActor.ACTOR;
+        return dslContext.select(
+                                 DSL.row(FILM.fields()),
+                                 DSL.row(FILM_ACTOR.fields()),
+                                 DSL.row(ACTOR.fields())
+                         )
+                         .from(FILM)
+                         .join(FILM_ACTOR)
+                         .on(FILM.FILM_ID.eq(FILM_ACTOR.FILM_ID))
+                         .join(ACTOR)
+                         .on(FILM_ACTOR.ACTOR_ID.eq(ACTOR.ACTOR_ID))
+                         .limit(pageSize)
+                         .offset((page - 1) * pageSize)
+                         .fetchInto(FilmWithActor.class);
     }
 }
